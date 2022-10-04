f23e03250872fbe1e6867e2c033df91f0891a36e
IlyassElmoutaoukkil <31111186+IlyassElmoutaoukkil@users.noreply.github.com>
2022-10-04 13:30:37 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+let score = 1
+let move = 'toRight'
+let snakePosition = [0,0]
+
+basic.forever(function () {
+    let leds = []
+    let s = 0
+
+	if(move=='toRight'){
+        while (s < score) {
+            s++
+            let ledNewPosition = [[snakePosition[0]++, [snakePosition[1]]
+            leds.push(ledNewPosition)
+        }
+    }
+})
