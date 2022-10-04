2d1a4f451dcb8c0766f509c865b9d08d1f5b452e
IlyassElmoutaoukkil <31111186+IlyassElmoutaoukkil@users.noreply.github.com>
2022-10-04 16:53:49 +0100

    all direction is now working

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 let ledNewPosition = []
 let score = 5
-let move = "toLeft"
-let lastMoveX = 'toRight'
+let move = "toTop"
+let lastMoveX = 'toLeft'
 let lastMoveY = 'toRight'
-let snakePosition = [[0, 0],[1,0],[2,0]]
+let snakePosition = [[1, 4],[2,4],[3,4]]
 let s=0
 basic.forever(function () {
     s++
@@ -14,17 +14,7 @@ basic.forever(function () {
         let snakeNewX = snakePosition[snakePosition.length - 1][0]+1
         snakePosition.push([snakeNewX, snakePosition[snakePosition.length - 1][1]])
         snakePosition.removeAt(0)
-        if (snakePosition[0][0]>5){
-            
-            let newX = 0 - snakePosition.length
-            let reSnake = [[newX, snakePosition[0][1]]]
-            for(let i=1;i<snakePosition.length;i++){
-                newX++
-                reSnake.push([newX, snakePosition[0][1]])
-            }
-
-            snakePosition = reSnake
-        }
+        
     }
 
     if (move == "toLeft") {
@@ -45,13 +35,61 @@ basic.forever(function () {
         }
     }
 
-    
 
+    if (move == "toBottom") {
+        if(lastMoveX=='toRight'){
+            let snakeNewY = snakePosition[snakePosition.length - 1][1] + 1
+            snakePosition.push([snakePosition[snakePosition.length - 1][0], snakeNewY])
+            snakePosition.removeAt(0)
+        }
 
-    
-    
+        if (lastMoveX == 'toLeft') {
+            let snakeNewY = snakePosition[0][1] + 1
+            snakePosition.unshift([snakePosition[0][0], snakeNewY])
+            snakePosition.removeAt(snakePosition.length-1)
+            
+        }
+
+        if (snakePosition[0][1] > 5) {
+
+            let newY = 0 - snakePosition.length
+            let reSnake = [[snakePosition[0][0],newY]]
+            for (let i = 1; i < snakePosition.length; i++) {
+                newY++
+                reSnake.push([snakePosition[0][0], newY])
+            }
+            snakePosition = reSnake
+        }
+    }
+
+    if (move == "toTop") {
+        if (lastMoveX == 'toRight') {
+            let snakeNewY = snakePosition[snakePosition.length - 1][1] - 1
+            snakePosition.push([snakePosition[snakePosition.length - 1][0], snakeNewY])
+            snakePosition.removeAt(0)
+        }
+
+        if (lastMoveX == 'toLeft') {
+            let snakeNewY = snakePosition[0][1] - 1
+            snakePosition.unshift([snakePosition[0][0], snakeNewY])
+            snakePosition.removeAt(snakePosition.length - 1)
+
+        }
+
+        if (snakePosition[snakePosition.length-1][1] < 0) {
+
+            let newY = 5 + snakePosition.length
+            let reSnake = [[snakePosition[0][0], newY]]
+            for (let i = 1; i < snakePosition.length; i++) {
+                newY--
+                reSnake.push([snakePosition[0][0], newY])
+            }
+            snakePosition = reSnake
+        }
+    }
+
     for (let i = 0; i <= snakePosition.length - 1; i++) {
-        console.log(snakePosition[i])
+        // console.log(snakePosition[i][1])
         led.plot(snakePosition[i][0], snakePosition[i][1])
     }
 
