50e6025a051e7a82eb973e37f36b4108a499fde7
IlyassElmoutaoukkil <31111186+IlyassElmoutaoukkil@users.noreply.github.com>
2022-10-04 18:01:58 +0100

    game working, radio added, stickGame added

--- main.ts
+++ main.ts
@@ -1,20 +1,55 @@
 let ledNewPosition = []
-let score = 5
-let move = "toTop"
+let score = 0
+let move = "toRight"
 let lastMoveX = 'toLeft'
 let lastMoveY = 'toRight'
-let snakePosition = [[1, 4],[2,4],[3,4]]
+let snakePosition = [[1, 0],[2,0],[3,0]]
 let s=0
+let food = [4,4]
+radio.setGroup(1)
 basic.forever(function () {
+    
+    if (pins.analogReadPin(AnalogPin.P0) > 1000) {
+        // to toBottom
+        move = 'toBottom'
+    }
+
+    if (pins.analogReadPin(AnalogPin.P0) < 10) {
+        // to toBottom
+        move = 'toTop'
+    }
+
+    if (pins.analogReadPin(AnalogPin.P1) < 10) {
+        // to toRight
+        move = 'toRight'
+    }
+
+    if (pins.analogReadPin(AnalogPin.P1) > 1000) {
+        // to toRight
+        move = 'toLeft'
+    }
+
     s++
     let leds = []
     basic.clearScreen()
+    led.plot(food[0], food[1])
     if (move == "toRight") {
-        
         let snakeNewX = snakePosition[snakePosition.length - 1][0]+1
         snakePosition.push([snakeNewX, snakePosition[snakePosition.length - 1][1]])
         snakePosition.removeAt(0)
-        
+        lastMoveX = 'toRight'
+
+        if (snakePosition[0][0] > 5) {
+
+            let newX = 0 - snakePosition.length
+            let reSnake = [[newX, snakePosition[0][1]]]
+            for (let i = 1; i < snakePosition.length; i++) {
+                newX++
+                reSnake.push([newX, snakePosition[0][1]])
+            }
+
+            snakePosition = reSnake
+        }
     }
 
     if (move == "toLeft") {
@@ -90,8 +125,21 @@ basic.forever(function () {
 
     for (let i = 0; i <= snakePosition.length - 1; i++) {
         // console.log(snakePosition[i][1])
+        if (snakePosition[i][0]==food[0] && snakePosition[i][1]==food[1]){
+            score =+ 1
+            radio.sendNumber(score)
+            GenerateFood()
+            led.plot(food[0], food[1])
+        }
+
         led.plot(snakePosition[i][0], snakePosition[i][1])
     }
 
-    basic.pause(400)
+    console.log(score)
+
+    basic.pause(100)
 })
+
+function GenerateFood(){
+    food = [Math.floor(Math.random() * (4 - 0 + 1)) + 0,Math.floor(Math.random() * (4 - 0 + 1)) + 0]
+}
