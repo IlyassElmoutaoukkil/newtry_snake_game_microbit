1764957d0f135da9d3390c31af09d305539ddb33
IlyassElmoutaoukkil <31111186+IlyassElmoutaoukkil@users.noreply.github.com>
2022-10-04 15:58:55 +0100

    Moving led only

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
+let ledNewPosition = []
 let score = 1
-let move = 'toRight'
-let snakePosition = [0,0]
-
+let move = "toRight"
+let snakePosition = [0, 0]
 basic.forever(function () {
     let leds = []
-    let s = 0
-
-	if(move=='toRight'){
+    basic.clearScreen()
+    if (move == "toRight") {
+        let s = 0
         while (s < score) {
-            s++
-            let ledNewPosition = [[snakePosition[0]++, [snakePosition[1]]
+            s += 1
+            let snakeX = snakePosition[0]
+            if (snakePosition[0] < 6){
+                snakeX = snakePosition[0]++
+            }else{
+                snakePosition[0]=0
+                snakeX = 0
+            }
+            ledNewPosition = [snakeX, snakePosition[1]]
             leds.push(ledNewPosition)
         }
     }
+    
+    for (let i = 0; i <= leds.length - 1; i++) {
+        console.log(leds[i])
+        led.plot(leds[i][0], leds[i][1])
+    }
+    basic.pause(400)
 })
